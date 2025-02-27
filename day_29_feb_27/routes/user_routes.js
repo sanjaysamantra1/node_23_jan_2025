724763d32708e729570fa95f608a151aa6e5ab89
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-02-27 07:59:46 +0530

    day 29

---
+++ day_29_feb_27/routes/user_routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { getAllUsers, get1User, addNewUser, updateUser, deleteUser } =
+    require('../controllers/user_controller');
+let userRouter = express.Router();
+
+userRouter.get('/', getAllUsers);
+userRouter.get('/:id', get1User);
+// userRouter.post('/', addNewUser);
+userRouter.patch('/:id', updateUser);
+userRouter.delete('/:id', deleteUser);
+userRouter.all('*',(req,res)=>{
+    res.send('NO such page found')
+})
+
+module.exports = userRouter;
