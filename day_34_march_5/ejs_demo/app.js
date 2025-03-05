3aed21eb3fff2060e3e59d1c4b28a0aeaae3721d
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-03-05 08:32:48 +0530

    ejs demo

--- day_34_march_5/ejs_demo/app.js
+++ day_34_march_5/ejs_demo/app.js
@@ -8,10 +8,16 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('pages/home.ejs');
 });
-app.get('/users', (req, res) => {
-    res.render('users.ejs', { users });
+app.get('/about', (req, res) => {
+    res.render('pages/about.ejs', { users });
+});
+app.get('/careers', (req, res) => {
+    res.render('pages/careers.ejs', { users });
+});
+app.get('/products', (req, res) => {
+    res.render('pages/products.ejs', { users });
 });
 
 app.listen(5000, () => {
